64634e403baecfaa229a91426d3a601c0c905d40
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/ASPxPivotGrid_SaveAndRestoreLayout/Default.aspx.vb
@@ -0,0 +1,18 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+
+Namespace SaveAndRestoreLayout
+	Partial Public Class _Default
+		Inherits System.Web.UI.Page
+		Protected Sub ASPxButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+			Session("Layout") = ASPxPivotGrid1.SaveLayoutToString()
+		End Sub
+		Protected Sub ASPxButton2_Click(ByVal sender As Object, ByVal e As EventArgs)
+			Dim layout As String = CStr(Session("Layout"))
+			If layout Is Nothing Then
+				Return
+			End If
+			ASPxPivotGrid1.LoadLayoutFromString(layout)
+		End Sub
+	End Class
+End Namespace
